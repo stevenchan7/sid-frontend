21a4f4a13195524d581fb2ba994423451ee29655
stevenchan7 <0stevenchan0@gmail.com>
2024-10-24 10:23:55 +0800

    change anouncement query to use tanstack

--- src/components/announcement/AnnouncementList.tsx
+++ src/components/announcement/AnnouncementList.tsx
@@ -6,75 +6,56 @@ import { Box, Skeleton, Text, VStack } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import AnnouncementCard from './AnnouncementCard';
+import { useQuery } from '@tanstack/react-query';
+import Announcement from '@/types/announcement.type';
 
 export default function AnnouncementList() {
 	const announcements = useAnnouncementStore((state) => state.announcements);
 	const setAnnouncements = useAnnouncementStore((state) => state.setAnnouncements);
 	const setAnnouncementsPaginate = useAnnouncementStore((state) => state.setAnnouncementsPaginate);
 	const search = useAnnouncementStore((state) => state.search);
-	const [title, setTitle] = useState('');
 	const [page, setPage] = useState(1);
-	const limit = 20;
+	const limit = 6;
 	const [hasMore, setHasMore] = useState(true);
-	const [isLoading, setIsLoading] = useState(true);
 
-	// const handleNext = async () => {
-	// 	try {
-	// 		const announcements = await getAnnouncement(page, limit, search);
-
-	// 		if (announcements.length > 0) {
-	// 			setAnnouncementsPaginate(announcements);
-	// 			setPage(page + 1);
-	// 		} else {
-	// 			setHasMore(false);
-	// 		}
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// };
-
-	function handleNext() {
+	function onNext() {
 		setPage(page + 1);
 	}
 
-	async function getAnnouncements(page: number, limit: number, search: string) {
-		try {
-			const announcements = await getAnnouncement({ page, limit, title: search });
+	const { data, isPending, refetch } = useQuery<Announcement[]>({
+		queryKey: ['announcements', 'all', page],
+		queryFn: () => getAnnouncement({ page, limit, title: search }),
+	});
+
+	useEffect(() => {
+		if (data) {
+			if (data.length < limit) {
+				setHasMore(false);
+			}
 
 			if (page === 1) {
-				setAnnouncements(announcements);
-			} else if (page > 1) {
-				setAnnouncementsPaginate(announcements);
-			}
-
-			if (announcements.length < limit) {
-				setHasMore(false);
+				setAnnouncements(data);
 			} else {
-				// Cek apakah ada pengumuman selanjutnya
-				const announcements = await getAnnouncement({ page: page + 1, limit, title: search });
-
-				if (announcements.length === 0) {
-					setHasMore(false);
-				}
+				setAnnouncementsPaginate(data);
 			}
-
-			setIsLoading(false);
-		} catch (error) {}
-	}
+		}
+	}, [data]);
 
 	useEffect(() => {
-		getAnnouncements(page, limit, search);
-	}, [page]);
-
-	useEffect(() => {
-		setPage(1);
-		setHasMore(true);
-		getAnnouncements(page, limit, search);
+		setPage(1); // Reset to page 1 when the search term changes
+		setHasMore(true); // Reset hasMore in case new search has more data
+		// Trigger data refetch based on new search term
+		refetch().then((result) => {
+			// Manually check if no results or fewer results than limit
+			if (!result.data || result.data.length < limit) {
+				setHasMore(false); // No more data to load
+			}
+		});
 	}, [search]);
 
 	return (
 		<Box flexBasis={'50%'} p={6} bg={'light-blue'} boxShadow={'base'} borderRadius={'lg'}>
-			{isLoading && (
+			{isPending && page === 1 && (
 				<VStack spacing={6} p={2}>
 					{Array.from({ length: 5 }).map((_, index) => (
 						<Skeleton key={index} w={'full'} h={'110px'}></Skeleton>
@@ -82,16 +63,20 @@ export default function AnnouncementList() {
 				</VStack>
 			)}
 
-			{announcements.length === 0 && !isLoading && <Text textAlign={'center'}>Tidak ada pengumuman</Text>}
+			{announcements.length === 0 && !isPending && <Text textAlign={'center'}>Tidak ada pengumuman</Text>}
 
 			{announcements.length > 0 && (
 				<Box id='scrollableParent' height={'600px'} overflowY={'scroll'}>
 					<InfiniteScroll
 						dataLength={announcements.length}
-						next={handleNext}
+						next={onNext}
 						hasMore={hasMore}
 						scrollableTarget='scrollableParent'
-						loader={<h4>Loading...</h4>}
+						loader={
+							<Text marginTop={6} textAlign={'center'}>
+								Loading
+							</Text>
+						}
 						endMessage={
 							<Text marginTop={6} textAlign={'center'}>
 								Tidak ada lagi pengumuman
